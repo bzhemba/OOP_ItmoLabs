16775d907798ee36f5b05d2bf4335bfe4e52016d
Bazhenova Mariia <113040756+bzhemba@users.noreply.github.com>
2023-09-28 23:18:26 +0300

    Delete src/Lab1/SpaceTravel/Services/ISpaceShipServices.cs
    style: unnecessary file removed

--- src/Lab1/SpaceTravel/Services/ISpaceShipServices.cs
+++
@@ -1,8 +0,0 @@
-using Itmo.ObjectOrientedProgramming.Lab1.SpaceTravel.Entities.SpaceShips;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.SpaceTravel.Services;
-
-public interface ISpaceShipServices
-{
-    public ISpaceShip GetTheBestOption(Augur? augur, Meridian? meridian, PleasureShuttle? pleasureShuttle, Stella? stella, int time);
-}
